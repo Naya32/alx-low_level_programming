7807daf9c4787ce825ca96a95cd752ad23d377e8
Naya32 <ezike.ifunanya@outlook.com>
2023-06-10 20:15:31 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
 
 /**
- * main - entry point of program.
- *
- * @argc: argument count.
- * @argv: argument vector.
- * program that prints number and new line.
- *
- * Return: 0 0n success.
- */
+*main - entry point of program.
+*
+* @argc: argument count.
+* @argv: argument vector.
+* program that prints number and new line.
+*
+* Return: 0 0n success.
+*/
 
 int main(int argc, char *argv[])
 {
 	int mount = argc - 1;
+	int argInt;
 
-	printf("number of argument : %d\n", mount);
+	if (argc > 1)
+	{
+		argInt = atoi(argv[1]);
+		printf("argument as an integer :  %d\n", argInt)
+	}
+
+	printf("number of argument :  %d\n", mount);
 
 	return (0);
 }

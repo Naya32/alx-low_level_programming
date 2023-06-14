c8188a4b7c856d31b100221d445c82f2e6411721
Naya32 <ezike.ifunanya@outlook.com>
2023-06-14 08:22:10 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 /**
  *main - program entry point.
  *
@@ -18,7 +17,7 @@ int main(int argc, char *argv[])
 
 if (argc != 3)
 {
-	printf("error\n");
+	printf("%s\n",argv[0]);
 	return (1);
 }
 

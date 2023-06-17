0514608c0a06006d58d701b43d7b057121474945
Naya32 <ezike.ifunanya@outlook.com>
2023-06-17 04:28:01 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -6,6 +6,7 @@
  * malloc_checked - Allocates memory using malloc.
  * @b: the number of bytes to allocate
  *
+ *
  * Return: pointer to memory or terminate with status 98 on faliure.
  *
  */
@@ -16,24 +17,7 @@ void *malloc_checked(unsigned int b)
 
 	if (ptr == NULL)
 	{
-		printf("error\n");
-			exit(98);
+		exit(98);
 	}
-
 	return (ptr);
-}
-
-/**
- * main - program entry point.
- *
- * Return: 0 on success.
- */
-
-int main(void)
-{
-	unsigned int size = 10;
-	int *arr = malloc_checked(size * sizeof(int));
-
-	free(arr);
-	return (0);
 }

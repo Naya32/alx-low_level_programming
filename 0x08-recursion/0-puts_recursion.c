0cc9e508fd73ccb9d7d84ae0502bdf47844307f2
naya32 <ezikeifunanya@outlook.com>
2024-01-10 15:41:01 -0800

    putchar

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,14 +1,25 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- *string_new_line - Prints a string followed by a new line
- * @str: The input string to be printed
+ * _putchar - Writes a character to the standard output
+ * @c: The character to print
  */
-void string_new_line(char *str)
+void _putchar(char c);
+
+/**
+ * _puts_recursion - Prints a string followed by a new line using recursion
+ * @s: The input string to be printed
+ */
+void _puts_recursion(char *s)
 {
-	if (str != NULL)
+	if (*s != '\0')
 	{
-		printf("%s\n", str);
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
 	}
 }
 

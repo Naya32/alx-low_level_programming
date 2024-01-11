f094a19cca54518f4e866bb4e29aec34bd5d7122
naya32 <ezikeifunanya@outlook.com>
2024-01-10 16:07:54 -0800

    new putchar

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,14 +1,9 @@
 #include "main.h"
 
 /**
- * _putchar - Writes a character to the standard output
- * @c: The character to print
- */
-void _putchar(char c);
-
-/**
  * _puts_recursion - Prints a string followed by a new line using recursion
  * @s: The input string to be printed
+ * return : nothing
  */
 void _puts_recursion(char *s)
 {

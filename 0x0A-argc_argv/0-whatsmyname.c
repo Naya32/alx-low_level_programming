1fdb88ef914873be1cb0d647b1f14276fd300eb1
naya32 <ezikeifunanya@outlook.com>
2024-01-14 16:30:39 -0800

    argument vector

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * @main Prints the name of the source file followed by a new line.
+ * @return 0 indicating successful execution.
+ */
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
+

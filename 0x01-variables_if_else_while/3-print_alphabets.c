ab8206ea3a51974b8389149a793e871b7d232955
Naya32 <ezike.ifunanya@outlook.com>
2025-03-14 06:40:42 -0700

    New Alphabet2

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+/*
+ * file created by Ezike Ifunanya
+ *
+ *  main: file to print both upper and lower case letters.
+ *  returns 0
+ */
+
+int main(void)
+{
+	char a;
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar (a);
+	}
+
+	for (a = 'A'; a <= 'Z'; a++)
+	{
+		putchar (a);
+	}
+	putchar ('\n');
+
+	return (0);
+}
